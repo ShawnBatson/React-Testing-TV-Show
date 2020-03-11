901d985a280bcdf4ac2d6a8ebd20df63aef1c9d6
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-03-11 18:34:52 -0500

    fixed your mistake

---
+++ src/api/fetchShow.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+
+const fetchShow = () => {
+  return axios.get(
+    "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+  );
+};
+
+export default fetchShow;
